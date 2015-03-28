0f82642cb70a3580ff396f423ef8fd18999ee368
mz2 <matias.piipari@gmail.com>
2015-03-28 19:03:25 +0000

    Adds KSFileUtilities.framework target

---
+++ KSFileUtilities/KSFileUtilities.h
@@ -0,0 +1,19 @@
+//
+//  KSFileUtilities.h
+//  KSFileUtilities
+//
+//  Created by Matias Piipari on 28/03/2015.
+//  Copyright (c) 2015 Jungle Candy Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KSFileUtilities.
+FOUNDATION_EXPORT double KSFileUtilitiesVersionNumber;
+
+//! Project version string for KSFileUtilities.
+FOUNDATION_EXPORT const unsigned char KSFileUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSFileUtilities/PublicHeader.h>
+
+
---
+++ KSFileUtilitiesTests/KSFileUtilitiesTests.m
@@ -0,0 +1,40 @@
+//
+//  KSFileUtilitiesTests.m
+//  KSFileUtilitiesTests
+//
+//  Created by Matias Piipari on 28/03/2015.
+//  Copyright (c) 2015 Jungle Candy Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface KSFileUtilitiesTests : XCTestCase
+
+@end
+
+@implementation KSFileUtilitiesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
